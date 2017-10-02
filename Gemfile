9ca02f1dd23a6915973d4c05d91bd787fb757cba
zawmoelwin <zawmoelwin@gmail.com>
2017-10-02 09:46:57 -0700

    changing first code

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source 'https://rubygems.org'
+
+# Specify your gem's dependencies in table_master.gemspec
+gemspec
+
+gem 'rails',        '>= 3.1.2'
---
+++ lib/table_master.rb
@@ -0,0 +1,32 @@
+require "table_master/version"
+
+module TableMaster
+  # Your code goes here...
+    class CreateClass
+        @table
+        @name
+        @model
+        
+        def createClass(name , model) 
+            @name = Object.const_set(name, Class.new(model))
+        end
+
+        def createTable(my_table, data_struct )
+            @table = my_table
+            sql = "CREATE TABLE #{my_table} (#{data_struct})"
+            ActiveRecord::Base.connection.execute(sql)
+              ActiveRecord::Base.connection.create_table
+            
+        end
+
+        def setTable(my_table)
+            @name.table_name = @table
+        end
+
+        def destroyClass
+            Object.send(:remove_const, @Name);
+        end
+
+    end    
+
+end
